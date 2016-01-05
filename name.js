59db0f7225ee6fdbcf235edc666f57fc9d458091
max33nau <mtj29@nau.edu>
2016-01-05 09:35:56 -0800

    test if name doesnt start with a number

--- name.js
+++ name.js
@@ -1,9 +1,11 @@
 var greeting = {
   sayHello: function(name) {
-    if (name) {
+    if (isNaN(name)) {
       return console.log('hello ' + name);
+    } else if (isNaN(name) == false) {
+      return console.log(name + ' is not a valid input');
     } else {
-      return console.log('a valid name was not entered');
+      return console.log('please try again');
     }
   }
 }
--- test.js
+++ test.js
@@ -1,11 +1,18 @@
 var assert = require('assert');
 var spawnSync = require('child_process').spawnSync;
 var greeting = require('./name');
+var should = require('chai').should();
 
 describe('test to check if the arguments are being processed', function(){
   it('should pass a argument on the command line', function(){
     var greet = spawnSync('node', ['greet.js', 'max']);
     var greetString = greet.stdout.toString();
     assert.deepEqual(greetString.slice(0,5), 'hello','did not get value from command line');
+  });
+  it('should not start with a number', function(){
+    var greet = spawnSync('node', ['greet.js', 'foo']);
+    var greetString = greet.stdout.toString();
+    var notNumber = isNaN(greetString.slice(0,1));
+    notNumber.should.equal(true);
   });
 })
