408422d5112e6055a6a08b7f186a42579da1a915
max33nau <mtj29@nau.edu>
2016-01-04 22:52:53 -0800

    argument passes test

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ var assert = require('assert');
 var spawnSync = require('child_process').spawnSync;
 var greeting = require('./name');
 
-describe('test to check if the user entered a name to greet', function(){
-  it('should pass a name on the command line', function(){
+describe('test to check if the arguments are being processed', function(){
+  it('should pass a argument on the command line', function(){
     var greet = spawnSync('node', ['greet.js', 'max']);
     var greetString = greet.stdout.toString();
     assert.deepEqual(greetString.slice(0,5), 'hello','did not get value from command line');
